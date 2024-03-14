6637c6670dd83582d0219ec5cafe6465023ad354
RemeMboup <marememboup93@gmail.com>
2024-03-14 12:35:47 +0000

    Retirer le dossier node_modules et du dossier config

--- config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-mongoose.connect(
-    process.env.MONGO_URI, {
-        useNewUrlParser : true,
-        useUnifiedTopology : true 
-    }
-).then(() => console.log("Connection à la base de donnees reussie")
-).catch((err) => console.log(err))
